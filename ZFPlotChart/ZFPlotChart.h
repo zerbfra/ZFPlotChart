d5003db624dd717c2176b23687578d7940e8f668
Zerbinati Francesco <me@zerbinatifrancesco.it>
2015-08-09 17:28:58 +0200

    Moved some definitions

--- ZFPlotChart/ZFPlotChart.h
+++ ZFPlotChart/ZFPlotChart.h
@@ -8,7 +8,30 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
-//#import "CPItem.h"
+#define COLOR_WITH_RGB(r,g,b)   [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0f] // Macro for colors
+
+// Dimension costants
+#define topMargin               20.0f   // top margin
+#define vMargin                 40.0f   // vertical margin
+#define hMargin                 10.0f   // horizontal margin
+#define leftSpace               10.0f   // left space
+
+// Graph grid color
+#define linesColor              [UIColor lightGrayColor]
+
+// Colors for lines and background
+#define orangeColor             COLOR_WITH_RGB(255, 150, 10)
+#define whiteColor              [UIColor whiteColor]
+
+// Fonts
+#define systemFont              [UIFont systemFontOfSize:10]
+#define boldFont                [UIFont boldSystemFontOfSize:10]
+
+// definitions (as JSON)
+#define fzPoint                  @"Point"
+#define fzValue                  @"value"
+#define fzDate                   @"time"
+#define fzDateFormat             @"yyyyMMdd"
 
 @interface ZFPlotChart : UIView
 {
--- ZFPlotChart/ZFPlotChart.m
+++ ZFPlotChart/ZFPlotChart.m
@@ -5,34 +5,6 @@
 //  Copyright (c) 2014-2015
 //
 
-#define COLOR_WITH_RGB(r,g,b)   [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0f] // Macro for colors
-
-// Dimension costants
-#define topMargin               20.0f   // top margin
-#define vMargin                 40.0f   // vertical margin
-#define hMargin                 10.0f   // horizontal margin
-#define leftSpace               10.0f   // left space
-
-// Graph grid color
-#define linesColor              [UIColor lightGrayColor]
-
-// Colors for lines and background
-#define orangeColor             COLOR_WITH_RGB(255, 150, 10)
-#define whiteColor              [UIColor whiteColor]
-
-// Fonts
-#define systemFont              [UIFont systemFontOfSize:10]
-#define boldFont                [UIFont boldSystemFontOfSize:10]
-
-// definitions (as JSON)
-#define fzPoint                  @"Point"
-#define fzValue                  @"value"
-#define fzDate                   @"time"
-#define fzDateFormat             @"yyyyMMdd"
-
-
-
-
 #import "ZFPlotChart.h"
 
 @implementation ZFPlotChart
